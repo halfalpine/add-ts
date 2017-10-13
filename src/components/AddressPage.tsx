760617ae13da04c499c826ca0d959d61643c685f
halfalpine <rocksandwoks@gmail.com>
2017-10-13 09:44:35 -0400

    Add page container styles

--- src/components/AddressPage.tsx
+++ src/components/AddressPage.tsx
@@ -1,9 +1,31 @@
 import * as React from "react";
+import styled from "styled-components";
 
 import { Entry } from "../models/model";
 import MenuBar from "./MenuBar";
 import AddressList from "./AddressList";
 import AddressForm from "./AddressForm";
+
+const StyledPageContainer = styled.div`
+  background: #83a4d4; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #b6fbff,
+    #83a4d4
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #b6fbff,
+    #83a4d4
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+const StyledAddressPage = styled.div`
+  margin: 10px;
+  padding: 10px;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 10px;
+`;
 
 interface Props {
   addresses: Entry[];
@@ -12,14 +34,16 @@ interface Props {
 }
 
 const AddressPage: React.StatelessComponent<Props> = props => (
-  <div>
-    <MenuBar />
-    <AddressList
-      addresses={props.addresses}
-      removeAddress={props.removeAddress}
-    />
-    <AddressForm addAddress={props.addAddress} />
-  </div>
+  <StyledPageContainer>
+    <StyledAddressPage>
+      <MenuBar />
+      <AddressList
+        addresses={props.addresses}
+        removeAddress={props.removeAddress}
+      />
+      <AddressForm addAddress={props.addAddress} />
+    </StyledAddressPage>
+  </StyledPageContainer>
 );
 
 export default AddressPage;
